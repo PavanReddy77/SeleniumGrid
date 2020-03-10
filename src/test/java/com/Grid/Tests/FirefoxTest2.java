6e07b2aa4bbd58e536831ec9c06de5132904fdf5
PavanReddy77 <Pavankrishnan1993@Gmail.com>
2020-03-10 22:51:19 +0530

    First Commit for Selenium-Grid

---
+++ src/test/java/com/Grid/Tests/FirefoxTest2.java
@@ -0,0 +1,44 @@
+package com.Grid.Tests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class FirefoxTest2 
+{
+	public static WebDriver driver;
+	
+	@Test
+	public void firefoxTest2() throws MalformedURLException
+	{		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("firefox");
+		capabilities.setPlatform(Platform.WINDOWS);
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.merge(capabilities);
+		
+		String hubUrl = "http://localhost:4444/wd/hub";
+		driver = new RemoteWebDriver(new URL(hubUrl), chromeOptions);
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+			
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.amazon.com");
+		
+		String title = driver.getTitle();
+		System.out.println("The FirefoxTest_2 Amazon Title is ::: " +title);
+		
+		driver.quit();
+	}
+} 
